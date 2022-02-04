b04911250593c8a0a227731008bf5ae960b43edd
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2022-02-04 16:37:59 -0300

    feat: add get wd path func

--- src/main.go
+++ src/main.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path"
+	"path/filepath"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
@@ -15,21 +17,41 @@ import (
 	"google.golang.org/api/option"
 )
 
-// Retrieve a token, saves the token, then returns the generated client.
-func getClient(config *oauth2.Config) *http.Client {
-	// The file token.json stores the user's access and refresh tokens, and is
-	// created automatically when the authorization flow completes for the first
-	// time.
-	tokFile := "token.json"
-	tok, err := tokenFromFile(tokFile)
+/**
+ * Get full wd path of specified relative path
+ */
+func getFullPath(relativePath string) string {
+	wd, err := os.Getwd()
+
 	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(tokFile, tok)
+		panic(err.Error())
 	}
-	return config.Client(context.Background(), tok)
+
+	rootDir := filepath.Dir(wd)
+
+	return path.Join(rootDir, relativePath)
 }
 
-// Request a token from the web, then returns the retrieved token.
+/**
+ * Retrieve a token, saves the token, then returns the generated client.
+ * The file token.json stores the user's access and refresh tokens, and is
+ * created automatically when the authorization flow completes for the first time.
+ */
+func getClient(config *oauth2.Config) *http.Client {
+	tokenFile := getFullPath("../token.json")
+	token, err := tokenFromFile(tokenFile)
+
+	if err != nil {
+		token = getTokenFromWeb(config)
+		saveToken(tokenFile, token)
+	}
+
+	return config.Client(context.Background(), token)
+}
+
+/**
+ * Request a token from the web, then returns the retrieved token.
+ */
 func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
 	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
 	fmt.Printf("Go to the following link in your browser then type the "+
@@ -40,26 +62,30 @@ func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
 		log.Fatalf("Unable to read authorization code %v", err)
 	}
 
-	tok, err := config.Exchange(context.TODO(), authCode)
+	token, err := config.Exchange(context.TODO(), authCode)
 	if err != nil {
 		log.Fatalf("Unable to retrieve token from web %v", err)
 	}
-	return tok
+	return token
 }
 
-// Retrieves a token from a local file.
+/**
+ * Retrieves a token from a local file.
+ */
 func tokenFromFile(file string) (*oauth2.Token, error) {
 	f, err := os.Open(file)
 	if err != nil {
 		return nil, err
 	}
 	defer f.Close()
-	tok := &oauth2.Token{}
-	err = json.NewDecoder(f).Decode(tok)
-	return tok, err
+	token := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(token)
+	return token, err
 }
 
-// Saves a token to a file path.
+/**
+ * Saves a token to a file path.
+ */
 func saveToken(path string, token *oauth2.Token) {
 	fmt.Printf("Saving credential file to: %s\n", path)
 	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
@@ -72,24 +98,29 @@ func saveToken(path string, token *oauth2.Token) {
 
 func main() {
 	ctx := context.Background()
-	b, err := ioutil.ReadFile("credentials.json")
+
+	b, err := ioutil.ReadFile(getFullPath("../credentials.json"))
+
 	if err != nil {
 		log.Fatalf("Unable to read client secret file: %v", err)
 	}
 
 	// If modifying these scopes, delete your previously saved token.json.
 	config, err := google.ConfigFromJSON(b, drive.DriveMetadataReadonlyScope)
+
 	if err != nil {
 		log.Fatalf("Unable to parse client secret file to config: %v", err)
 	}
-	client := getClient(config)
 
-	srv, err := drive.NewService(ctx, option.WithHTTPClient(client))
+	client := getClient(config)
+	driveService, err := drive.NewService(ctx, option.WithHTTPClient(client))
+
 	if err != nil {
 		log.Fatalf("Unable to retrieve Drive client: %v", err)
 	}
 
-	r, err := srv.Files.List().PageSize(40).Fields("nextPageToken, files(id, name)").Do()
+	filesResponse, err :=
+		driveService.Files.List().PageSize(40).Fields("nextPageToken, files(id, name)").Do()
 
 	if err != nil {
 		log.Fatalf("Unable to retrieve files: %v", err)
@@ -97,10 +128,10 @@ func main() {
 
 	fmt.Println("Files:")
 
-	if len(r.Files) == 0 {
+	if len(filesResponse.Files) == 0 {
 		fmt.Println("No files found.")
 	} else {
-		for _, i := range r.Files {
+		for _, i := range filesResponse.Files {
 			fmt.Printf("%s (%s)\n", i.Name, i.Id)
 		}
 	}
